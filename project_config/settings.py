68452b7874483b1f58a0e6ad1f0147bce717ad99
torhaa <torjus.haakens@outlook.com>
2025-06-20 20:08:40 +0200

    update timezone settings and git ignore

--- project_config/settings.py
+++ project_config/settings.py
@@ -104,11 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+TIME_ZONE = "Europe/Oslo"
 USE_I18N = True
-
 USE_TZ = True
 
 
